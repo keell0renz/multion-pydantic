4b69985bcaab6ef47593ebcb8729bbb0a2f18aad
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-08-02 15:38:34 +0200

    base

--- fastapi_example/fastapi_example/app.py
+++ fastapi_example/fastapi_example/app.py
@@ -4,6 +4,7 @@ from .multion_pydantic import MultiOn, MultiOnPydantic
 
 app = FastAPI()
 
+client: MultiOnPydantic = MultiOn(api_key="05ab569bdefe479aaa6077a59c06cf6c")  # type: ignore
 
 @app.get("/scrape_product_page")
 async def scrape_product_page():
