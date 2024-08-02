cb41cc5da3d1fd9108fb0191edff3e82515e3b8c
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-08-02 16:00:51 +0200

    Fix

--- fastapi_example/fastapi_example/queries.py
+++ fastapi_example/fastapi_example/queries.py
@@ -7,5 +7,5 @@ class ProductPageQuery(BaseModel):
     product_name: str
     pricing: float
     total_reviews: float
-    product_description: str | None = Field(description="Either return existing description if it is short, or if it is too large (100 words+) generate it yourself up to 100 words.")
-    product_details: str | None = Field(description="Provide here any additonal details you have found, if not -- you may generate it yourself based on page content.")
+    product_description: str = Field(description="Either return existing description if it is short, or if it is too large (100 words+) generate it yourself up to 100 words.")
+    product_details: str = Field(description="Provide here any additonal details you have found, if not -- you may generate it yourself based on page content.")
